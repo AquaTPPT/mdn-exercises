978b0be77e041b26957ccf959fdab527766ab9ab
AquaTPPT <195988559+AquaTPPT@users.noreply.github.com>
2025-07-22 19:45:43 +0100

    set up the project.

---
+++ JS/countdown.js
@@ -0,0 +1,15 @@
+const output = document.querySelector(".output");
+output.textContent = "";
+
+let i = 10;
+do {
+--i;
+console.log(i);
+}
+while (i > 1)
+
+// let i = 10;
+
+// const para = document.createElement('p');
+// para.textContent = ;
+// output.appendChild(para);
